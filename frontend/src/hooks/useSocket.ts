c794ffb972161beb23042288ed45aaa3e94866e3
Gaurav Nardia <gauravnardia07@gmail.com>
2025-04-18 14:39:18 +0530

    add base url

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const WS_URL = "ws://localhost:8000";
+const WS_URL = "https://chess-flmc.onrender.com";
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
