7859533d3223421ae8aae20955cd51f008608a43
Gaurav Nardia <gauravnardia07@gmail.com>
2025-04-18 14:15:23 +0530

    update settigns

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from './screens/Landing';
 import Game from './screens/Game';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className=' bg-slate-950'>
