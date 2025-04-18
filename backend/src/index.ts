4476f18efa13f4d4b23e722b220c1835523ba6df
Gaurav Nardia <gauravnardia07@gmail.com>
2025-04-18 13:26:45 +0530

    init http server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
+import  express from "express";
+import http from "http"
 import { WebSocketServer } from "ws";
 import { GameManager } from "./GameManager";
+
+const app = express();
+const server = http.createServer(app);
 
 const wss = new WebSocketServer({ port: 8000 });
 
@@ -10,3 +15,11 @@ wss.on('connection', function connection(ws){
     ws.on('disconnect', () => gameManager.removeUser(ws))
 })
 
+app.get("/", (_req, res) => {
+    res.send("WebSocket server is live on Render 🚀");
+  });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
